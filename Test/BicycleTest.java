115369bc8ebec103be48a611f81a8ae8f7bc39ca
Theo <52174070+theo1992@users.noreply.github.com>
2020-04-27 00:42:36 +0200

    BikeRide excercise

---
+++ Test/BicycleTest.java
@@ -0,0 +1,10 @@
+import bicycles.Bicycle;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class BicycleTest {
+        @Test
+        public void shouldAccellerate() {
+            Bicycle bicycle = new Bicycle();
+            assertEquals(5,5);
+        }
+    }
---
+++ src/bicycles/BikeShop.java
@@ -0,0 +1,8 @@
+package bicycles;
+public class BikeShop {
+    public static void main(String args[]) {
+            Bicycle B1 = new Bicycle();
+            //B1.accelerate(12);
+        }
+    }
+
