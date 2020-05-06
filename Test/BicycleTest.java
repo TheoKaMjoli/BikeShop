4d166e3b87da2d5ee084d942102cb84ff11ef6ab
Theo <52174070+theo1992@users.noreply.github.com>
2020-05-06 15:10:52 +0200

    We have added updates on Junit testing

--- Test/BicycleTest.java
+++ Test/BicycleTest.java
@@ -5,6 +5,16 @@ public class BicycleTest {
         @Test
         public void shouldAccellerate() {
             Bicycle bicycle = new Bicycle();
-            assertEquals(5,5);
+            assertEquals(5,5,"It has to balance");
         }
+        @Test
+        public void shouldBrake(){
+            Bicycle bicycle = new Bicycle();
+            assertEquals(5, 5, "It has to balance");
+        }
+        public void shouldCompleteStop(){
+            Bicycle bicycle = new Bicycle();
+            assertEquals(2, 2, "It has to balance");
+        }
+
     }
